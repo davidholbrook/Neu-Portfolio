49706ced62dda4c279642fb070c7c8263bf9efe0
David Holbrook <david@davidholbrook.info>
2018-02-17 11:52:59 -0600

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "settings": {
+        "ecmascript": 6,
+        "jsx": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "experementalDecorators": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+    ],
+    "extends": "airbnb",
+    "rules": {
+        "jsx-a11y/href-no-hash": "off",
+        "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    }
+};
