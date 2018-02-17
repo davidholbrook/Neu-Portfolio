16d1df1a5a87c421745e548d7b4316f73b6d8d50
David Holbrook <david@davidholbrook.info>
2018-02-17 13:24:53 -0600

    Added ESLinting

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,31 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
+  <div role="header" className="mainHead">
+      <h1>
+        <Link to="/">
+          David Holbrook
         </Link>
       </h1>
-    </div>
+      <nav className="mainNav">
+      <li>
+        <Link to="/">About</Link>
+      </li>
+      <li>
+        <Link to="/">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/">Contact</Link>
+      </li>
+      </nav>
   </div>
 )
 
