43a5a44b915245b7dba3957f95592d34b3c05278
David Holbrook <david@davidholbrook.info>
2018-02-17 21:22:09 -0600

    added nice scrolling experence

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+  <main>
+    <h1>Bender, I didn't know you liked cooking. That's so cute.</h1>
+    <p>But I know you in the future. I cleaned your poop. I don't want to be rescued. So I really am important? How I feel when I'm drunk is correct? Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew.</p>
+    <p>Hello, little man. I will destroy you! You mean while I'm sleeping in it? <strong> In our darkest hour, we can stand erect, with proud upthrust bosoms.</strong> <em> Yes, if you make it look like an electrical fire.</em> When you do things right, people won't be sure you've done anything at all.</p>
+    <h2>You guys aren't Santa! You're not even robots. How dare you lie in front of Jesus?</h2>
+    <p>I'll get my kit! Fry! Stay back! He's too powerful! Oh, I always feared he might run off like this. Why, why, why didn't I break his legs? Yes. You gave me a dollar and some candy. You'll have all the Slurm you can drink when you're partying with Slurms McKenzie!</p>
+    <ol>
 
-export default IndexPage
+      <li>It doesn't look so shiny to me.</li><li>Ah, yes! John Quincy Adding Machine. He struck a chord with the voters when he pledged not to go on a killing spree.</li><li>Son, as your lawyer, I declare y'all are in a 12-piece bucket o' trouble. But I done struck you a deal: Five hours of community service cleanin' up that ol' mess you caused.</li>
+
+    </ol>
+
+    <h3>Hey, guess what you're accessories to.</h3>
+    <p>Tell them I hate them. Fatal. Fry! Quit doing the right thing, you jerk! It doesn't look so shiny to me.</p>
+    <ul>
+
+      <li>Professor, make a woman out of me.</li><li>Hey, whatcha watching?</li><li>Kif might!</li>
+
+    </ul>
+
+    <p>Why not indeed! Your best is an idiot! Isn't it true that you have been paid for your testimony? Tell her she looks thin. Anyone who laughs is a communist! Oh, how awful. Did he at least die painlessly? …To shreds, you say. Well, how is his wife holding up? …To shreds, you say.</p>
+    <p>No. We're on the top. We can't compete with Mom! Her company is big and evil! Ours is small and neutral! Come, Comrade Bender! We must take to the streets! I could if you hadn't turned on the light and shut off my stereo.</p>
+    <p>When will that be? I videotape every customer that comes in here, so that I may blackmail them later. Why am I sticky and naked? Did I miss something fun? Bender?! You stole the atom. Have you ever tried just turning off the TV, sitting down with your children, and hitting them?</p>
+    <p>Why am I sticky and naked? Did I miss something fun? Leela, Bender, we're going grave robbing. You lived before you met me?! I love this planet! I've got wealth, fame, and access to the depths of sleaze that those things bring.</p>
+    <p>Whoa a real live robot; or is that some kind of cheesy New Year's costume? And I'm his friend Jesus. What kind of a father would I be if I said no? No, of course not. It was… uh… porno. Yeah, that's it.</p>
+    <p>Look, last night was a mistake. THE BIG BRAIN AM WINNING AGAIN! I AM THE GREETEST! NOW I AM LEAVING EARTH, FOR NO RAISEN! Anyone who laughs is a communist! I wish! It's a nickel.</p>
+    <p>One hundred dollars. You wouldn't. Ask anyway! Good man. Nixon's pro-war and pro-family. Who said that? SURE you can die! You want to die?! Bender, we're trying our best. Why would a robot need to drink?</p>
+    <p>Okay, I like a challenge. No! The kind with looting and maybe starting a few fires! You guys aren't Santa! You're not even robots. How dare you lie in front of Jesus? Why would a robot need to drink? They're like sex, except I'm having them!</p>
+    <p>So I really am important? How I feel when I'm drunk is correct? These old Doomsday Devices are dangerously unstable. I'll rest easier not knowing where they are. This is the worst kind of discrimination: the kind against me!</p>
+    <p>I'll tell them you went down prying the wedding ring off his cold, dead finger. Oh, I always feared he might run off like this. Why, why, why didn't I break his legs? Then throw her in the laundry room, which will hereafter be referred to as "the brig".</p>
+    <p>Bender, we're trying our best. Isn't it true that you have been paid for your testimony? Okay, it's 500 dollars, you have no choice of carrier, the battery can't hold the charge and the reception isn't very…</p>
+  </main>
+);
+
+export default IndexPage;
