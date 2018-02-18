ca1f7568bb15e4d57a74c3c1e6fa9cb888b90a59
David Holbrook <david@davidholbrook.info>
2018-02-17 21:14:53 -0600

    Added text to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const AboutDavid = () => (
-  <div>
+  <main>
     <h2>About David Holbrook</h2>
-    <p>Hi, I'm Dave, a front-end designer from Minneapolis, Minnesota. I have over fifteen years of experience in creating websites and front-end applications for small and medium-sized businesses. I recently graduated with a Bachelors in Entrepreneurship Studies as MCAD (Minneapolis College of Art and Design) and I love working with business and products of all kinds and helping them succeed online. Let me know what I can do for you next thing, email me at david@davidholbrook.info.</p>
-  </div>
+    <p>Hi, I'm Dave, a front-end designer from Minneapolis, Minnesota. I have over fifteen years of experience in creating websites and front-end applications for small and medium-sized businesses. I recently graduated with a Bachelors in Entrepreneurship Studies as MCAD (Minneapolis College of Art and Design) and I love working with business and products of all kinds and helping them succeed online. Let me know what I can do for your next thing, email me at david@davidholbrook.info.</p>
+  </main>
 );
 
 export default AboutDavid;
