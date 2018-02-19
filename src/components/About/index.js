54ada3a1374f1ac23ceefdf7646329bfc1406aa4
David Holbrook <david@davidholbrook.info>
2018-02-19 01:09:37 -0600

    misc. tweaks

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link';
 const AboutMe = () => (
   <div>
     <p className="intro">My name is David, a front-end designer from Maple Grove, Minnesota. I have been creating websites for the last 10 years and having fun doing it.</p>
-    <Link to="/about" className="aboutLink">About Me</Link>
+    <Link to="/about" className="aboutLink">More About Me</Link>
   </div>
 );
 
