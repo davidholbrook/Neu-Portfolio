e3c75a25a2007441adab081ac4146147b7e2c7b2
David Holbrook <david@davidholbrook.info>
2018-02-20 15:08:19 -0600

    added portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import Link from 'gatsby-link';
 
 import ProjectCard from '../components/ProjectCard';
 
 // import in all the logos.
 import SpiritedFeet from '../layouts/images/client/spiritedfeet.png';
 import cgos from '../layouts/images/client/CGOS.png';
-import applebarn from '../layouts/images/client/applebarn.png';
 import mcad from '../layouts/images/client/mcad.png';
+import messagecab from '../layouts/images/client/messageCab.png';
+import replan from '../layouts/images/client/replan.png';
+import applebarn from '../layouts/images/client/applebarn.png';
 
 
-const IndexPage = () => (
+const Portfolio = () => (
   <main className="fullwidth">
-    <h2 className="title">Featured Projects</h2>
+    <h2 className="title">All Projects</h2>
     <div className="ProjectCard__wrapper">
       <ProjectCard logo={SpiritedFeet} bgColor="#142440" link="project/spiritedfeet">
         <h2>Spirited Feet</h2>
@@ -30,11 +31,16 @@ const IndexPage = () => (
         <h2>Minneapolis College of Art and Design</h2>
         <p>As I was completing my Bachelor's degree at Minneapolis College of Art and Design, I had the opportunity to work with the web communications department at my school. Working with this department, I was responsible for many different tasks and projects, including everything from researching new features, building new additions, and maintaining the current website for the school.</p>
       </ProjectCard>
+      <ProjectCard logo={messagecab} bgColor="#221f20">
+        <h2>Message Cab</h2>
+        <p>Selling cards and posters were the dream for a client of mine as a side business. The client had the idea for the website and the inventory to sell. He needed my technical skills to reach a global audience with his product.</p>
+      </ProjectCard>
+      <ProjectCard logo={replan} bgColor="#a642fe">
+        <h2>Replan</h2>
+        <p>Bender, I didn't know you liked cooking. That's so cute. Then throw her in the laundry room, which will hereafter be referred to as "the brig". As an interesting side note, as a head without a body, I envy the dead. We can't compete with Mom! Her company is big and evil! Ours is small and neutral!</p>
+      </ProjectCard>
     </div>
-    <Link to="/portfolio" className="portfolioLink">View all Case Studies</Link>
-    <h2 className="title">Contact Me</h2>
-    <p>Insert form here</p>
   </main>
 );
 
-export default IndexPage;
+export default Portfolio;
