0f908b8f98a29dd80ccf225f911ab30f050c9a62
David Holbrook <david@davidholbrook.info>
2018-02-21 00:15:40 -0600

    Added titles for Project.metadata

--- src/layouts/projects.js
+++ src/layouts/projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 
 export default function Template({ data }) {
   const { markdownRemark: project } = data;
@@ -7,10 +8,12 @@ export default function Template({ data }) {
     <main className="grid">
       <Helmet title={`${project.frontmatter.title} - David Holbrook`} />
       <div className="Project__meta">
-        <h2>{project.frontmatter.title}</h2>
+        <h2 className="Project__title">{project.frontmatter.title}</h2>
+        <h3>ROLE</h3>
         <p>{project.frontmatter.role}</p>
+        <h3>URL</h3>
         {project.frontmatter.url
-          ? <p>{project.frontmatter.url}</p>
+          ? <p><Link to="{project.frontmatter.url}">{project.frontmatter.url}</Link></p>
           : <p>No website available</p>
         }
       </div>
