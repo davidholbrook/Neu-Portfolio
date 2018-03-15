cb301a1fc994a92d5e0c53ce718b46557f1cad46
David Holbrook <david@davidholbrook.info>
2018-03-15 13:21:41 -0500

    Header mobile changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
 import Logo from '../../layouts/images/Logo';
 
-const Header = () => (
-  <div role="Banner" className="mainHead">
-    <Link to="/">
-      <h1 className="logo">
-        <Logo width="120" height="120" />
-      </h1>
-    </Link>
-    <nav className="mainNav">
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link to="/">Contact</Link>
-      </li>
-    </nav>
-  </div>
-);
+export default class Header extends Component {
+  state = {
+    toggl: false,
+  };
 
-export default Header;
+  toggle = () => {
+    this.setState({ toggl: !this.state.toggl });
+  };
+
+  render() {
+    return (
+      <div role="Banner" className="mainHead">
+        <Link to="/">
+          <h1 className="logo">
+            <Logo width="120" height="120" />
+          </h1>
+        </Link>
+        <nav className="mainNav navDesktop">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </nav>
+        <nav className="mobileNav">
+          <button onClick={this.toggle} className="mobile__opener">
+            Open Navagation
+          </button>
+          {this.state.toggl === true ? (
+            <div className="mobile__toggl">
+              <div className="mobile__list">
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                  <Link to="/">Contact</Link>
+                </li>
+                <li>
+                  <a onClick={this.toggle}>Close</a>
+                </li>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+        </nav>
+      </div>
+    );
+  }
+}
