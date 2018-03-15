9419e1533b2eb9bfaf0f608e44db27d8c1488623
David Holbrook <david@davidholbrook.info>
2018-03-15 12:06:04 -0500

    Revised wording

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React from "react";
+import Link from "gatsby-link";
 
 const AboutMe = () => (
   <div>
-    <p className="intro">My name is David, a front-end designer from Maple Grove, Minnesota. I have been creating websites for the last 10 years and having fun doing it.</p>
-    <Link to="/about" className="aboutLink">More About Me</Link>
+    <p className="intro">
+      Hi I'm David, a front-end designer from Minnesota. I have been creating
+      websites for the last 10 years and love doing it.
+    </p>
+    <Link to="/about" className="aboutLink">
+      More About Me
+    </Link>
   </div>
 );
 
 export default AboutMe;
-
