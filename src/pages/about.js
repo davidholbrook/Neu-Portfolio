415c4b0a27bf596a6085694f11b7402214689103
David Holbrook <david@davidholbrook.info>
2018-03-16 15:25:12 -0500

    added background to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import Link from "gatsby-link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AboutDavid = () => (
   <div>
-    <div className="background--black">
+    <div className="background--about">
       <Header />
+      <main>
+        <h2>Write Code. Build Businesses.</h2>
+        <p>
+          Hi, I'm Dave, a front-end designer from Minneapolis, Minnesota. I have
+          over fifteen years of experience in creating websites and front-end
+          applications for small and medium-sized businesses. I recently
+          graduated with a Bachelors in Entrepreneurship Studies as MCAD
+          (Minneapolis College of Art and Design) and I love working with
+          business and products of all kinds and helping them succeed online.
+          Let me know what I can do for your next thing, email me at
+          david@davidholbrook.info.
+        </p>
+      </main>
     </div>
-    <main>
-      <h2>About David Holbrook</h2>
-      <p>
-        Hi, I'm Dave, a front-end designer from Minneapolis, Minnesota. I have
-        over fifteen years of experience in creating websites and front-end
-        applications for small and medium-sized businesses. I recently graduated
-        with a Bachelors in Entrepreneurship Studies as MCAD (Minneapolis
-        College of Art and Design) and I love working with business and products
-        of all kinds and helping them succeed online. Let me know what I can do
-        for your next thing, email me at david@davidholbrook.info.
-      </p>
-    </main>
     <Footer />
   </div>
 );
