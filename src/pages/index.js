3cde86b4472d693d73408f8f4bd0fa4080aacc9a
David Holbrook <david@davidholbrook.info>
2018-03-19 13:08:29 -0500

    changed copy on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,11 @@ const IndexPage = () => (
       <ProjectCard logo={applebarn} bgColor="#981B38" link="project/applebarn">
         <h2>Apple Barn Orchard and Winery</h2>
         <p>
-          Apple Barn is a family owned farm located in Elkhorn, Wisconsin. They
-          specialize in fresh Strawberries, Pumpkins and many different
-          varieties of apples. Families can come to the farm and expect a fun
-          experience of picking up the freshest fruit.
+          Apple Barn is a family-owned farm located in Elkhorn, Wisconsin. They
+          specialize in fresh Strawberries, Pumpkins, many different varieties
+          of Apples, and wine. Families of all kinds can come to the farm and
+          expect a fun experience of picking up the freshest fruit and sample
+          great wine at the winery.
         </p>
       </ProjectCard>
       <ProjectCard
@@ -28,16 +29,16 @@ const IndexPage = () => (
       >
         <h2>Spirited Feet</h2>
         <p>
-          Spirited Feet is a dancing organization based in Woodbury, Minnesota.
-          Their mission is to teach dance and Christ in a non-competitive dance
-          environment. They teach dance and choreography to grades 3 through 12
-          and offered many nights a week.
+          Spirited Feet is a dance organization based in Woodbury, Minnesota.
+          Their mission is to teach dance and christian teaching in a
+          non-competitive environment. They teach grades 3 through 12 and offer
+          classes many nights a week.
         </p>
       </ProjectCard>
     </div>
-    <Link to="/portfolio" className="portfolioLink">
+    {/* <Link to="/portfolio" className="portfolioLink">
       View all Case Studies
-    </Link>
+    </Link> */}
     <h2 className="title">Let's Chat</h2>
     <p>
       Did you find something that you want to hire me to do, or just want to
