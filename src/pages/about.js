dbf265e088cff61616335b3e9f89562abecf686a
David Holbrook <david@davidholbrook.info>
2018-03-23 23:31:26 -0500

    content rewrites

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,14 +10,12 @@ const AboutDavid = () => (
       <div className="content">
         <h2>Write Code. Build Businesses.</h2>
         <p>
-          Hi, I'm Dave, a front-end designer from Minneapolis, Minnesota. I have
-          over fifteen years of experience in creating websites and front-end
+          I'm Dave, a front-end designer from Minneapolis, Minnesota. I have
+          over 10 years of experience in creating websites and front-end
           applications for small and medium-sized businesses. I recently
           graduated with a Bachelors in Entrepreneurship Studies as MCAD
           (Minneapolis College of Art and Design) and I love working with
           business and products of all kinds and helping them succeed online.
-          Let me know what I can do for your next thing, email me at
-          david@davidholbrook.info.
         </p>
 
         <div className="about__graphic">
