1d09a646f31df1a183e2ef4b674f22414ef69320
David Holbrook <david@davidholbrook.info>
2018-03-26 14:04:06 -0500

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Link from 'gatsby-link';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="background--black">
+      <Header />
+    </div>
+    <main className="fourohfour">
+      <h2>404 NOT FOUND</h2>
+      <p>
+        Sorry, I cannot find the page you are looking for, try finding it from
+        the <Link to="/">homepage</Link>.
+      </p>
+    </main>
+    <Footer />
   </div>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
